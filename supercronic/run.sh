f9263f7d751aa29c9e90ffc023c18f18412486c2
tomneo2004 <tomneo2004@gmail.com>
2025-05-10 15:44:40 +0800

    --allow-empty-message

--- supercronic/run.sh
+++ supercronic/run.sh
@@ -10,6 +10,8 @@ CRONTAB_DIR="/etc/crontab"
 CRONTAB_FILE="$CRONTAB_DIR/crontab"
 DEFAULT_CRONTAB_FILE="./crontab_default"
 
+# if crontab file not exits then
+# copy defult crontab file to crontab directory
 if [[ ! -f $CRONTAB_FILE ]]; then 
   echo "crontabl doesn't exists, use default crontab"
   cp $DEFAULT_CRONTAB_FILE $CRONTAB_FILE
@@ -17,4 +19,5 @@ else
   echo "Use external crontab $CRONTAB_FILE"
 fi
 
+# run crontab file
 supercronic -inotify $CRONTAB_FILE
