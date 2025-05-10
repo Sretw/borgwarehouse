928a214e0183eb61eba2f3426482fb29bbbf246c
tomneo2004 <tomneo2004@gmail.com>
2025-05-10 14:54:24 +0800

    --allow-empty-message

--- supercronic/run.sh
+++ supercronic/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-print_yellow() {
-  echo -e "\e[33m$1\e[0m";
-}
+set -e
+
+# print_yellow() {
+#   echo -e "\e[33m$1\e[0m";
+# }
 
 if [[ -z "$EXTERNAL_CRONTAB_PATH" ]]; then 
-  print_yellow "Use default crontab $DEFAULT_CRONTAB_PATH"
+  echo "Use default crontab $DEFAULT_CRONTAB_PATH"
   supercronic -inotify $DEFAULT_CRONTAB_PATH
 else
+  echo "Use external crontab $EXTERNAL_CRONTAB_PATH"
   supercronic -inotify $EXTERNAL_CRONTAB_PATH
 fi
