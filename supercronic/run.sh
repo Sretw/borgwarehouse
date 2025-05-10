18588736c17c55794d7f23ea831096504ad17aff
tomneo2004 <tomneo2004@gmail.com>
2025-05-10 13:57:18 +0800

    --allow-empty-message

--- supercronic/run.sh
+++ supercronic/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+print_yellow() {
+  echo -e "\e[33m$1\e[0m";
+}
+
 if [[ -z "${CRONTAB_PATH}" ]]; then 
-    echo copy default crontab 
-    cp crontab ./crontab 
-    ${CRONTAB}=./crontab 
+    print_yellow "copy default crontab" 
+    ${CRONTAB}=./crontab_default
+    print_yellow "crontab path ${CRONTAB}" 
 fi
